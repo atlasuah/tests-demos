48e6decc0f365aae1629af91caef3fde110c4012
BC <bac0015@uah.edu>
2013-03-20 20:12:52 -0500

    Added mapper demo

---
+++ MapperTest/MapperTest/MapperDemo.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace MapperTest
+{
+    partial class MapperDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbMap = new System.Windows.Forms.PictureBox();
+            this.btnScale = new System.Windows.Forms.Button();
+            this.tbScale = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbBumpX = new System.Windows.Forms.TextBox();
+            this.tbBumpY = new System.Windows.Forms.TextBox();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.tbPosX = new System.Windows.Forms.TextBox();
+            this.tbPosY = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPointCount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMap
+            // 
+            this.pbMap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbMap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pbMap.Location = new System.Drawing.Point(12, 12);
+            this.pbMap.Name = "pbMap";
+            this.pbMap.Size = new System.Drawing.Size(503, 507);
+            this.pbMap.TabIndex = 0;
+            this.pbMap.TabStop = false;
+            this.pbMap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbMap_MouseMove);
+            this.pbMap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbMap_MouseClick);
+            this.pbMap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbMap_MouseDown);
+            this.pbMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbMap_MouseUp);
+            this.pbMap.SizeChanged += new System.EventHandler(this.pbMap_SizeChanged);
+            // 
+            // btnScale
+            // 
+            this.btnScale.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnScale.Location = new System.Drawing.Point(595, 52);
+            this.btnScale.Name = "btnScale";
+            this.btnScale.Size = new System.Drawing.Size(75, 23);
+            this.btnScale.TabIndex = 1;
+            this.btnScale.Text = "Set Scale";
+            this.btnScale.UseVisualStyleBackColor = true;
+            this.btnScale.Click += new System.EventHandler(this.btnScale_Click);
+            // 
+            // tbScale
+            // 
+            this.tbScale.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tbScale.Location = new System.Drawing.Point(607, 26);
+            this.tbScale.Name = "tbScale";
+            this.tbScale.Size = new System.Drawing.Size(63, 20);
+            this.tbScale.TabIndex = 2;
+            this.tbScale.Text = "1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSave.Location = new System.Drawing.Point(595, 157);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save Map";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbBumpX
+            // 
+            this.tbBumpX.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tbBumpX.Location = new System.Drawing.Point(607, 231);
+            this.tbBumpX.Name = "tbBumpX";
+            this.tbBumpX.Size = new System.Drawing.Size(63, 20);
+            this.tbBumpX.TabIndex = 4;
+            // 
+            // tbBumpY
+            // 
+            this.tbBumpY.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tbBumpY.Location = new System.Drawing.Point(608, 258);
+            this.tbBumpY.Name = "tbBumpY";
+            this.tbBumpY.Size = new System.Drawing.Size(62, 20);
+            this.tbBumpY.TabIndex = 5;
+            // 
+            // btnZero
+            // 
+            this.btnZero.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnZero.Location = new System.Drawing.Point(595, 313);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(75, 23);
+            this.btnZero.TabIndex = 6;
+            this.btnZero.Text = "Center Shift";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // tbPosX
+            // 
+            this.tbPosX.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tbPosX.Location = new System.Drawing.Point(604, 382);
+            this.tbPosX.Name = "tbPosX";
+            this.tbPosX.Size = new System.Drawing.Size(66, 20);
+            this.tbPosX.TabIndex = 7;
+            // 
+            // tbPosY
+            // 
+            this.tbPosY.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tbPosY.Location = new System.Drawing.Point(604, 408);
+            this.tbPosY.Name = "tbPosY";
+            this.tbPosY.Size = new System.Drawing.Size(66, 20);
+            this.tbPosY.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnClear.Location = new System.Drawing.Point(595, 128);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear Map";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnSet.Location = new System.Drawing.Point(595, 284);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 10;
+            this.btnSet.Text = "Set Shift";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(564, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Scale:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(546, 411);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Mouse Y:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(546, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Mouse X:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(560, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Shift X:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(561, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Shift Y:";
+            // 
+            // tbPointCount
+            // 
+            this.tbPointCount.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.tbPointCount.Location = new System.Drawing.Point(604, 478);
+            this.tbPointCount.Name = "tbPointCount";
+            this.tbPointCount.Size = new System.Drawing.Size(66, 20);
+            this.tbPointCount.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(559, 481);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Points:";
+            // 
+            // MappingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 537);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbPointCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.tbPosY);
+            this.Controls.Add(this.tbPosX);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.tbBumpY);
+            this.Controls.Add(this.tbBumpX);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbScale);
+            this.Controls.Add(this.btnScale);
+            this.Controls.Add(this.pbMap);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(700, 565);
+            this.Name = "MappingForm";
+            this.Text = "Mapping Demo";
+            this.Load += new System.EventHandler(this.MappingForm_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MappingForm_KeyUp);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MappingForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMap;
+        private System.Windows.Forms.Button btnScale;
+        private System.Windows.Forms.TextBox tbScale;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbBumpX;
+        private System.Windows.Forms.TextBox tbBumpY;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.TextBox tbPosX;
+        private System.Windows.Forms.TextBox tbPosY;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPointCount;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MapperTest/MapperTest/MappingForm.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+//          --  Usage  --
+//
+//  1. Ctrl  + Click             -    Add point to map.
+//  2. Scroll Wheel              -    Adjust scale of map.
+//  3. Click + Drag              -    Move around the map.
+//  2. Shift + Click + Drag      -    Scaled Dragging for moving the map.
+//                                      Dragging ignores scale factor.
+//                                      Useful when scale gets very large or small.
+
+namespace MapperTest
+{
+    public partial class MappingForm : Form
+    {
+        float oldPosX = 0;
+        float oldPosY = 0;
+        int prevScaleBumpX = 0,
+            prevScaleBumpY = 0;
+        int bumpX = 0,
+            bumpY = 0;
+        int oldBumpX = 0,
+            oldBumpY = 0;
+        int oldMouseX = 0,
+            oldMouseY = 0;
+        int gridTickSize = 50,
+            gridTickCount = 30,
+            gridStartX = -500,          // Start/Stop X and Y are adjusted in Form1_Load
+            gridStartY = -500,
+            gridStopX = 500,
+            gridStopY = 500;
+        int pointCount = 1;
+        List<Point> listPoints;
+        List<Point> listGridPoints;
+        Bitmap bmp;
+        float scale = 1;
+        bool firstPoint = true;
+        bool mouseDown = false;
+        bool drawNewPoint = false;
+        bool scaleDrag = false;
+
+        public MappingForm()
+        {
+            InitializeComponent();
+            this.MouseWheel += new MouseEventHandler(pbMap_MouseWheel);
+            listPoints = new List<Point>();
+            listGridPoints = new List<Point>();
+        }
+        private void MappingForm_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pbMap.Width, pbMap.Height);
+
+            // Center the grid
+            gridStartX = -1 * ((gridTickCount * gridTickSize / 2) - (pbMap.Width / 2));
+            gridStartY = -1 * ((gridTickCount * gridTickSize / 2) - (pbMap.Height / 2));
+            gridStopX = (gridTickCount * gridTickSize / 2) + (pbMap.Width / 2);
+            gridStopY = (gridTickCount * gridTickSize / 2) + (pbMap.Height / 2);
+
+            DrawMap();
+        }
+
+        // When new encoder data comes in
+        private void pbMap_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!firstPoint && drawNewPoint)
+            {
+                oldPosX = (float)(e.X - (bumpX * scale)) / scale;
+                oldPosY = (float)(e.Y - (bumpY * scale)) / scale;
+                listPoints.Add(new Point((int)oldPosX, (int)oldPosY));
+
+                oldPosX = e.X;
+                oldPosY = e.Y;
+
+                pointCount++;
+                tbPointCount.Text = "" + pointCount;
+
+                DrawMap();
+            }
+            else if (drawNewPoint)
+            {
+                oldPosX = (float)(e.X - (bumpX * scale)) / scale;
+                oldPosY = (float)(e.Y - (bumpY * scale)) / scale;
+                listPoints.Add(new Point((int)oldPosX, (int)oldPosY));
+                oldPosX = e.X;
+                oldPosY = e.Y;
+                firstPoint = false;
+            }
+        }
+        private void pbMap_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            oldMouseX = e.X;
+            oldMouseY = e.Y;
+            // If the shift key is held, we need to adjust the old pos.
+            //   Otherwise the map will jump around like an idiot.
+            if (scaleDrag)
+            {
+                oldMouseX = (int)(oldMouseX / scale);
+                oldMouseY = (int)(oldMouseY / scale);
+            }
+            if (!drawNewPoint)
+                Cursor = Cursors.SizeAll;
+            else if (!firstPoint)
+                Cursor = Cursors.Default;
+        }
+        private void pbMap_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+            oldBumpX = bumpX;
+            oldBumpY = bumpY;
+            Cursor = Cursors.Default;
+        }
+        private void pbMap_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Drag map around
+            if (mouseDown && !drawNewPoint)
+            {
+                if (scaleDrag)
+                {
+                    bumpX = (int)((oldBumpX - (oldMouseX - (e.X / scale))));
+                    bumpY = (int)((oldBumpY - (oldMouseY - (e.Y / scale))));
+                }
+                else
+                {
+                    bumpX = (oldBumpX - (oldMouseX - e.X));
+                    bumpY = (oldBumpY - (oldMouseY - e.Y));
+                }
+                tbBumpX.Text = "" + bumpX;
+                tbBumpY.Text = "" + bumpY;
+                DrawMap();
+            }
+            //else if (drawNewPoint && !firstPoint)     // Drag and Draw
+            //{
+            //    oldPosX = (float)(e.X - (bumpX * scale)) / scale;
+            //    oldPosY = (float)(e.Y - (bumpY * scale)) / scale;
+            //    listPoints.Add(new Point((int)oldPosX, (int)oldPosY));
+            //    DrawMap();
+            //}
+            tbPosX.Text = "" + e.X;
+            tbPosY.Text = "" + e.Y;
+        }
+        private void pbMap_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // Re-centers the map after adjusting the scale
+            prevScaleBumpX = (int)(((pbMap.Width / 2) - ((pbMap.Width / 2) / scale)));
+            prevScaleBumpY = (int)(((pbMap.Height / 2) - ((pbMap.Height / 2) / scale)));
+            if (e.Delta > 0)
+            {
+                scale *= 1.25F;
+                bumpX += (-1 * (int)(((pbMap.Width / 2) - ((pbMap.Width / 2) / scale)))) + prevScaleBumpX;
+                bumpY += (-1 * (int)(((pbMap.Height / 2) - ((pbMap.Height / 2) / scale)))) + prevScaleBumpY;
+            }
+            else
+            {
+                scale /= 1.25F;
+                bumpX -= (int)(((pbMap.Width / 2) - ((pbMap.Width / 2) / scale))) - prevScaleBumpX;
+                bumpY -= (int)(((pbMap.Height / 2) - ((pbMap.Height / 2) / scale))) - prevScaleBumpY;
+            }
+
+            oldBumpX = bumpX;
+            oldBumpY = bumpY;
+
+            tbBumpX.Text = "" + bumpX;
+            tbBumpY.Text = "" + bumpY;
+            tbScale.Text = "" + scale;
+            DrawMap();
+        }
+
+        private void MappingForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                bumpX += 5;
+                tbBumpX.Text = "" + bumpX;
+                tbBumpY.Text = "" + bumpY;
+                DrawMap();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                bumpX -= 5;
+                tbBumpX.Text = "" + bumpX;
+                tbBumpY.Text = "" + bumpY;
+                DrawMap();
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                bumpY += 5;
+                tbBumpX.Text = "" + bumpX;
+                tbBumpY.Text = "" + bumpY;
+                DrawMap();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                bumpY -= 5;
+                tbBumpX.Text = "" + bumpX;
+                tbBumpY.Text = "" + bumpY;
+                DrawMap();
+            }
+            else if (e.KeyCode == Keys.ControlKey)
+                drawNewPoint = true;
+            else if (e.KeyCode == Keys.ShiftKey)
+                scaleDrag = true;
+        }
+        private void MappingForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+                drawNewPoint = false;
+            if (e.KeyCode == Keys.ShiftKey)
+                scaleDrag = false;
+        }
+
+        private void DrawMap()
+        {
+            // Draw grid background
+            try
+            {
+                bmp = new Bitmap(pbMap.Width, pbMap.Height);
+                scale = (float)Convert.ToDouble(tbScale.Text);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    for (int i = 0; i <= gridTickCount; i++)
+                    {
+                        g.DrawLine(new Pen(Color.Blue, 1),
+                            (gridStartX + (i * gridTickSize) + bumpX) * scale,
+                            (gridStartY + 0 + bumpY) * scale,
+                            (gridStartX + (i * gridTickSize) + bumpX) * scale,
+                            (gridStopY + bumpY) * scale);
+                    }
+                    for (int i = 0; i <= gridTickCount; i++)
+                    {
+                        g.DrawLine(new Pen(Color.Blue, 1),
+                            (gridStartX + 0 + bumpX) * scale,
+                            (gridStartY + (i * gridTickSize) + bumpY) * scale,
+                            (gridStopX + bumpX) * scale,
+                            (gridStartY + (i * gridTickSize) + bumpY) * scale);
+                    }
+                }
+                pbMap.Image = bmp;
+            }
+            catch (Exception) { }
+
+            // Draw path/map
+            if (listPoints.Count > 0)
+            {
+                try
+                {
+                    //bmp = new Bitmap(pbMap.Width, pbMap.Height);
+                    scale = (float)Convert.ToDouble(tbScale.Text);
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                        for (int i = 0; i < (listPoints.Count - 1); i++)
+                        {
+                            g.DrawLine(new Pen(Color.Black, (int)(5 * scale)),
+                                (listPoints[i].X + bumpX) * scale,
+                                (listPoints[i].Y + bumpY) * scale,
+                                (listPoints[i + 1].X + bumpX) * scale,
+                                (listPoints[i + 1].Y + bumpY) * scale);
+                        }
+                    }
+                    pbMap.Image = bmp;
+                    oldPosX = (listPoints[listPoints.Count - 1].X + bumpX) * scale;
+                    oldPosY = (listPoints[listPoints.Count - 1].Y + bumpY) * scale;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            bmp.Save("test.bmp");
+        }
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            bumpX = -1 * (int)(((pbMap.Width / 2) - ((pbMap.Width / 2) / scale)));
+            bumpY = -1 * (int)(((pbMap.Height / 2) - ((pbMap.Height / 2) / scale)));
+            oldMouseX = 0;
+            oldMouseY = 0;
+            DrawMap();
+            tbBumpX.Text = "" + bumpX;
+            tbBumpY.Text = "" + bumpY;
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listPoints.Clear();
+            pointCount = 1;
+            tbPointCount.Text = "0";
+            firstPoint = true;
+            DrawMap();
+        }
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            bumpX = Convert.ToInt32(tbBumpX.Text);
+            bumpY = Convert.ToInt32(tbBumpY.Text);
+            DrawMap();
+        }
+        private void btnScale_Click(object sender, EventArgs e)
+        {
+            DrawMap();
+        }
+
+        private void pbMap_SizeChanged(object sender, EventArgs e)
+        {
+            DrawMap();
+        }
+    }
+}
